d825f7e76a27ffb0365fcb2884b09475d17f4594
Noèlia Llobera Sànchez <noelialloberas@gmail.com>
2022-08-10 12:17:02 +0200

    Ejercicio 3

---
+++ script.js
@@ -0,0 +1,21 @@
+let cadena = "string";
+let numero = 1;
+let boleano = "true";
+let nulo = null;
+let indefinido;
+let objeto = {
+    nombre: "Noèlia",
+    apellido: "Llobera",
+}
+let array = [0,1,2,3,4,5];
+// let simbolo = Symbol("Símbolo");
+
+
+console.log(`Tipo: ${typeof(cadena)}, valor: ${cadena}`);
+console.log(`Tipo: ${typeof(numero)}, valor: ${numero}`);
+console.log(`Tipo: ${typeof(boleano)}, valor: ${boleano}`);
+console.log(`Tipo: ${typeof(nulo)}, valor: ${nulo}`);
+console.log(`Tipo: ${typeof(indefinido)}, valor: ${indefinido}`);
+console.log(`Tipo: ${typeof(objeto)}, valor: ${objeto}`);
+console.log(`Tipo: ${typeof(array)}, valor: ${array}`);
+// console.log(`Tipo: ${typeof(simbolo)}, valor: ${simbolo}`);
